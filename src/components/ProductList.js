d9655784b0d9a6dc73fe785bb5a97480b1b8af82
Gilbert Gamboa <gilbert.g.alvarado@gmail.com>
2020-04-20 23:06:48 -0600

    store1
    ProductList, ProductContext: ProductProvider & ProductConsumer

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,24 +1,30 @@
-import React, { Component } from "react";
-import Product from "./Product";
-import Title from "./Title";
+import React, { Component } from 'react';
+import { ProductConsumer } from '../context';
+
+import Product from './Product';
+import Title from './Title';
 
 export default class ProductList extends Component {
-  state = {
-    products: [],
-  };
-
   render() {
     return (
       <React.Fragment>
-       {/* 16*: 5=3, 4=1.5, 3=16, 2=.5, 1=.25, 0=0 */}
-        <div className="py-5"> 
+        {/* 16*: 5=3, 4=1.5, 3=16, 2=.5, 1=.25, 0=0 */}
+        <div className="py-5">
           <div className="container">
             <Title name="our" title="products" />
-            <div className="row"></div>
+            <div className="row">
+              <ProductConsumer>
+                {/* Return the value that's on ProductContext.Provider */}
+                {(value) => {
+                  return value.products.map((product) => {
+                    return <Product key={product.id} product={product}/>;
+                  });
+                }}
+              </ProductConsumer>
+            </div>
           </div>
         </div>
       </React.Fragment>
-      // <Product/>
     );
   }
 }
